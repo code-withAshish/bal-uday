88857b3aa959acf0facc68204ca49338038b5ed1
Ashish Bhushan Kumar <73625149+code-withAshish@users.noreply.github.com>
2024-06-16 21:03:36 +0530

    push to github

---
+++ src/components/ImageCarousel.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { Splide, SplideSlide } from "@splidejs/svelte-splide";
+  import '@splidejs/splide/dist/css/themes/splide-default.min.css';
+</script>
+
+<Splide
+  aria-label="My Favorite Images"
+  options={{perPage:3,rewind:true}}
+>
+
+  <SplideSlide>
+    <img src={"/Carousel1.png"} alt="an hero" />
+  </SplideSlide>
+  <SplideSlide>
+    <img src={"/Carousel2.png"} alt="an hero" />
+  </SplideSlide>
+  <SplideSlide>
+    <img src={"/Carousel3.png"} alt="an hero" />
+  </SplideSlide>
+</Splide>
+
