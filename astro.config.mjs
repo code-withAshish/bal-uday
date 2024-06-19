f0e9bf56f05d8c682f56187a1c965f099eea3d13
Ashish Bhushan Kumar <73625149+code-withAshish@users.noreply.github.com>
2024-06-19 12:45:31 +0530

    attach form

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 
-import netlify from "@astrojs/netlify";
-
 // https://astro.build/config
 export default defineConfig({
   output: "static",
